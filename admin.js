c81992f455c1ddfa34f3b99d02af492345b59c66
haze888 <username@example.com>
2024-02-15 17:35:41 +0200

    Add admin functionalities. Add forms to modify posts.

--- admin.js
+++ admin.js
@@ -1,2 +1,62 @@
 // Admin app to add/edit/delete new book notes
-// Only admin can view
+// Only admin can use this app
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = new express();
+const port = 4000;
+
+app.use(express.static('public'));
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "notes",
+  password: "postgres",
+  port: 5432,
+});
+
+db.connect();
+
+let notes = [];
+
+function viewNotes() {
+  db.query("SELECT * FROM note", (err, res) => {
+    if(err) {
+      console.error("Error executing query", err.stack);
+    } else {
+      notes = res.rows;
+    }
+  });
+}
+
+viewNotes();
+
+// View the main page
+app.get("/", async (req, res) => {
+  viewNotes();
+  res.render("admin-index.ejs", {notes: notes});
+});
+
+// GET the new post form
+app.get("/new", (req, res) => {
+  res.render("modify.ejs");
+});
+
+// GET post by id
+app.get("/:id", (req, res) => {
+  const note = notes.find((note) => note.id === parseInt(req.params.id));
+  res.render("note.ejs", {note: note});
+});
+
+// Edit certain post
+app.get("/edit/:id", (req, res) => {
+  const note = notes.find((note) => note.id === parseInt(req.params.id));
+
+  res.render("modify.ejs", {note: note});
+});
+
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`);
+});
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // App that is visible to all users
 import express from "express";
-import bodyParser from "body-parser";
 import pg from "pg";
 
 const app = new express();
@@ -8,22 +7,22 @@ const port = 3000;
 
 app.use(express.static('public'));
 
-let notes = [
-  {
-    isbn: "9780805210408",
-    title: "The Trial by Franz Kafka",
-    dateRead: "February 12th 2022",
-    description: "On the morning of his thirtieth birthday, Josef K., the chief clerk of a bank, is unexpectedly arrested by two unidentified agents from an unspecified agency for an unspecified crime. Josef is not imprisoned, however, but left \"free\" and told to await instructions from the Committee of Affairs. Josef's landlady, Frau Grubach, tries to console Josef about the trial, but insinuates that the procedure may be related to an immoral relationship with his neighbor Fräulein Bürstner. Josef visits Bürstner to vent his worries, and then kisses her.",
-  },
-  {
-    isbn: "9781329437982",
-    title: "Frankenstein by Mary Shelley",
-    dateRead: "January 27th 2022",
-    description: "Shelley's novel, Frankenstein: or, the Modern Prometheus (1818), is a combination of Gothic horror story and science fiction. The book tells the story of Victor Frankenstein, a Swiss student of natural science who creates an artificial man from pieces of corpses and brings his creature to life.",
-  },
-]
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "notes",
+  password: "postgres",
+  port: 5432,
+});
 
-app.get("/", (req, res) => {
+db.connect();
+
+let notes = [];
+
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT * FROM note");
+  notes = result.rows;
+
   res.render("index.ejs", {notes: notes});
 });
 
